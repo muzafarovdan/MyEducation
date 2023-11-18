24fa48b528a78d3addf6aa585090bd5be35377a0
Muzafarov Danil <muzafarov.danil@mail.ru>
2023-11-18 16:53:52 +0300

    add: new Solutions

--- ADS/HW4/HW4_D.py
+++ ADS/HW4/HW4_D.py
@@ -1,104 +1,82 @@
-import sys
-# Определение класса для узла дека
 class Node:
-    def __init__(self, data):
-        self.data = data
-        self.next = None
+    def __init__(self, value):
+        self.value = value
         self.prev = None
+        self.next = None
 
-# Определение класса для дека
 class Deque:
     def __init__(self):
         self.front = None
-        self.rear = None
+        self.back = None
 
-    def is_empty(self):
-        return self.front is None
-    
-    def add_front(self, data):
-        new_node = Node(data)
-        
-        if self.is_empty():
-            self.front = self.rear = new_node
+    def push_front(self, value):
+        new_node = Node(value)
+        if not self.front:
+            self.front = self.back = new_node
         else:
             new_node.next = self.front
             self.front.prev = new_node
             self.front = new_node
-    
-    def add_rear(self, data):
-        new_node = Node(data)
-        
-        if self.is_empty():
-            self.front = self.rear = new_node
+
+    def push_back(self, value):
+        new_node = Node(value)
+        if not self.back:
+            self.front = self.back = new_node
         else:
-            new_node.prev = self.rear
-            self.rear.next = new_node
-            self.rear = new_node
-    
-    def remove_front(self):
-        if self.is_empty():
-            return None
-        
-        if self.front == self.rear:
-            temp = self.front
-            self.front = self.rear = None
-        else:
-            temp = self.front
-            self.front = self.front.next
+            new_node.prev = self.back
+            self.back.next = new_node
+            self.back = new_node
+
+    def pop_front(self):
+        value = self.front.value
+        self.front = self.front.next
+        if self.front:
             self.front.prev = None
-            
-        return temp.data
-    
-    def remove_rear(self):
-        if self.is_empty():
-            return None
-        
-        if self.front == self.rear:
-            temp = self.rear
-            self.front = self.rear = None
         else:
-            temp = self.rear
-            self.rear = self.rear.prev
-            self.rear.next = None
-            
-        return temp.data
+            self.back = None
+        return value
 
-# Создание дека
-deque_1 = Deque()
-deque_2 = Deque()
+    def pop_back(self):
+        value = self.back.value
+        self.back = self.back.prev
+        if self.back:
+            self.back.next = None
+        else:
+            self.front = None
+        return value
 
-for _ in range(int(input())):
-    test = sys.stdin.readline().split()
-    if test[0] == 'pushfront':
-        if test[1] == 1:
-            deque_1.add_front(test[2])
-        elif test[1] == 2:
-            deque_2.add_front(test[2])
-    elif test[0] == 'pushback':
-        if test[1] == 1:
-            deque_1.add_rear(test[2])
-        elif test[1] == 2:
-            deque_2.add_rear(test[2])
-    elif test[0] == 'popfront':
-        if test[1] == 1:
-            print(deque_1.remove_front())
-        if test[1] == 2:
-            print(deque_2.remove_front())
-    elif test[0] == 'popback':
-        if test[1] == 1:
-            print(deque_1.remove_rear())
-            print('Hello')
-        if test[1] == 2:
-            print(deque_2.remove_rear())
-            
+# Функция для обработки команд
+def process_commands(commands):
+    deque_dict = {}
+    result = []
 
-# # Добавление элементов в начало и конец дека
-# my_deque.add_front(10)
-# my_deque.add_rear(20)
+    for command in commands:
+        cmd_parts = command.split()
+        operation = cmd_parts[0]
 
-# # Удаление элементов из начала и конца дека
-# item1 = my_deque.remove_front()
-# item2 = my_deque.remove_rear()
+        if operation.startswith('push'):
+            _, deque_id, value = cmd_parts
+            value = int(value)
+            if deque_id not in deque_dict:
+                deque_dict[deque_id] = Deque()
+            if operation == 'pushfront':
+                deque_dict[deque_id].push_front(value)
+            elif operation == 'pushback':
+                deque_dict[deque_id].push_back(value)
+        elif operation.startswith('pop'):
+            _, deque_id = cmd_parts
+            if operation == 'popfront':
+                result.append(deque_dict[deque_id].pop_front())
+            elif operation == 'popback':
+                result.append(deque_dict[deque_id].pop_back())
 
-# Вывод дека
-# print(my_deque)
+    return result
+
+# Ввод данных
+n = int(input())
+commands = [input() for _ in range(n)]
+
+# Обработка команд и вывод результата
+result = process_commands(commands)
+for value in result:
+    print(value)
