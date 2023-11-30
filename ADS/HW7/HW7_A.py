d80e74a9c65d6bdc7651afb31897354a1ac31b99
Muzafarov Danil <muzafarov.danil@mail.ru>
2023-11-30 22:21:23 +0300

    add:new HW

---
+++ ADS/HW7/HW7_A.py
@@ -0,0 +1,21 @@
+import itertools
+
+tests = int(input())
+n,m = map(int, input().split())
+
+for test in range(tests):
+    num = list(map(int, input().split()))
+    perm_set = itertools.permutations(num)
+    found_value = False
+    for num_i in perm_set:
+        s = 0
+        for i in range(len(num_i)):
+            s += sum([num_i[i+j] / (i+j+1) for j in range(len(num_i)-i)])
+            if s == m:
+                print('YES')
+                found_value = True
+                break
+        if found_value:
+            break
+   
+    
