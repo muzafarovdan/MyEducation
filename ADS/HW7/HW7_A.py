efd64c4c08bc8d48cd0cead85fd36268e919ca6c
Muzafarov Danil <muzafarov.danil@mail.ru>
2023-12-03 14:32:24 +0300

    upd: solution

--- ADS/HW7/HW7_A.py
+++ ADS/HW7/HW7_A.py
@@ -1,21 +1,9 @@
-import itertools
-
 tests = int(input())
-n,m = map(int, input().split())
 
 for test in range(tests):
+    n,m = map(int, input().split())
     num = list(map(int, input().split()))
-    perm_set = itertools.permutations(num)
-    found_value = False
-    for num_i in perm_set:
-        s = 0
-        for i in range(len(num_i)):
-            s += sum([num_i[i+j] / (i+j+1) for j in range(len(num_i)-i)])
-            if s == m:
-                print('YES')
-                found_value = True
-                break
-        if found_value:
-            break
-   # не проходит
-    
+    if sum(num) != m:
+        print("NO")
+    else:
+        print("YES")
