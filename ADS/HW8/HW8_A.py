6d078f1ac3c5ed04d07ba3d2aec1201d9bb3d3a4
Muzafarov Danil <muzafarov.danil@mail.ru>
2023-12-10 17:20:10 +0300

    add: new file and one solution

--- ADS/HW8/HW8_A.py
+++ ADS/HW8/HW8_A.py
@@ -1,13 +1,21 @@
 tests = int(input())
 
 for test in range(tests):
-    n, x = map(int,input().split())
-    a = list(map(int,input().split()))
-    b = list(map(int,input().split()))
+    while True:
+        try:
+            n, x = map(int, input().split())
+            break
+        except ValueError:
+            pass
+
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
     for i in range(n):
-        if a[i]+b[n-1-i] <= x:
+        if a[i] + b[n - 1 - i] <= x:
             continue
         else:
             print('No')
             break
-    print('Yes')
+    else:
+        print('Yes')
